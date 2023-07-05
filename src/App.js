b223e5371e18d9da1b01632094cc51127783cdde
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-07-05 13:24:30 +0900

    생활코딩 React 공부
    생활코딩 React 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+
+function Header(props) {
+  //console.log('props', props.title);
+  return <header>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault(); // a태그가 동작하는 기본동작을 막는것
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'},
+  ]
+  return(
+    <div>
+      <Header title="WEB" onChangeMode={function(){
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+      <Article title="Welcome" body="Hello WEB"></Article>
     </div>
   );
 }
