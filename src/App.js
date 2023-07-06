dbab4ffb0fb2ff59da54dcd953996984f16e177a
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-07-06 13:40:22 +0900

    Component와 State
    Component와 State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
+import {useState} from 'react';
 
 function Header(props) {
   //console.log('props', props.title);
@@ -22,7 +23,12 @@ function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -32,20 +38,45 @@ function Nav(props){
 }
 
 function App() {
+  //const _mode = useState('WELCOME');
+  //const mode = _mode[0];
+  //const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME'); // 위 세줄이랑 같은 코드
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'},
   ]
+
+  let content = null;
+
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello WEB"></Article>
+  } else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return(
     <div>
-      <Header title="WEB" onChangeMode={function(){
-        alert('Header');
+      <Header title="WEB" onChangeMode={()=>{
+        //alert('Header');
+        setMode('WELCOME'); // 값을 바꿀때는 setMode를 사용
       }}></Header>
       <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+        //alert(id);
+        setMode('READ');
+        setId(id);
       }}></Nav>
-      <Article title="Welcome" body="Hello WEB"></Article>
+      {content}
     </div>
   );
 }
